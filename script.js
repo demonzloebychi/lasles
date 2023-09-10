d5adab84d8afa9764bedc70b0b2a2cdcb4644204
Nikolai <107766413+demonzloebychi@users.noreply.github.com>
2023-09-10 22:24:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ $(document).ready(function(){
     });
 });
 
+const burger = document.querySelector('.header_burger');
+const menu = document.querySelector('.header_li_main');
+const link = document.querySelector('.menu__item')
+
+
+if (burger){
+    burger.addEventListener("click", function(e){
+        document.body.classList.toggle('lock');
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+
+    menu.querySelectorAll('.menu__item').forEach(link => {
+        link.addEventListener('click', function(e) {
+            document.body.classList.remove('lock');
+            menu.classList.remove('active');
+            burger.classList.remove('active');
+        })
+    })
+    
+}
+
+
 
 const anchors = document.querySelectorAll("a[href*='#']")
 
